dc644d82e1e703d22bf2e300ba6df2d2743d347b
rouz majlessi <rmajlessi@gmail.com>
2016-08-08 02:11:01 -0700

    test 2 pass set name

--- Tests/VenueTest.cs
+++ Tests/VenueTest.cs
@@ -13,16 +13,32 @@ namespace BandTracker.Objects
      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
    }
 
+
    [Fact]
    public void Test1_GetVenueName()
    {
-     // arrange
+
      Venue newVenue = new Venue("O2");
-     // act
+
      string result = newVenue.GetVenueName();
 
      Assert.Equal("O2", result);
    }
+
+
+   // [Fact]
+  public void Test2_SetVenueName()
+  {
+    // arrange
+    Venue newVenue = new Venue("Skydome");
+    newVenue.SetName("Skydome");
+    // act
+    string result = newVenue.GetVenueName();
+
+    Assert.Equal("Skydome", result);
+  }
+
+
 
 
   }
